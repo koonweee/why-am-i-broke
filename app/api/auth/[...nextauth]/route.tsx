6a8aeda13ce67c9df976323433d60a27175fca48
Jeremy Tan <jeremy.tan@newfront.com>
2024-07-22 19:18:56 -0700

    Fix route export

--- app/api/auth/[...nextauth]/route.tsx
+++ app/api/auth/[...nextauth]/route.tsx
@@ -2,5 +2,4 @@ import { authOptions } from "@/app/api/auth/[...nextauth]/options";
 import NextAuth from "next-auth/next";
 
 const handler = NextAuth(authOptions);
-const { signIn } = handler;
-export { handler as GET, handler as POST, signIn };
+export { handler as GET, handler as POST };
